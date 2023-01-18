c31a62d84ab0e1630ce70d950573a9b53fc5d0c3
hanle9 <han.levan199@gmail.com>
2023-01-18 14:52:21 +0700

    feat: Update header
    https://github.com/unikbase/home/issues/78

--- src/layout/UserLayout.tsx
+++ src/layout/UserLayout.tsx
@@ -1,8 +1,9 @@
 import { IconButton } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { MdLogout } from "react-icons/md";
+import { HiChevronLeft } from "react-icons/hi";
 import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import { addToken, removeToken } from "../store/slices/authSlice";
 import { RootState } from "../store/store";
 import APIService from "../services/APIService";
@@ -10,6 +11,9 @@ import APIService from "../services/APIService";
 const UserLayout = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const [isShowBackButton, setIsShowBackButton] = useState(false);
+  const location = useLocation();
+
   const { refreshToken, accessToken } = useSelector(
     (state: RootState) => state.auth
   );
@@ -19,6 +23,10 @@ const UserLayout = () => {
   const onLogout = () => {
     dispatch(removeToken({}));
     navigate("/token-login", { replace: true });
+  };
+
+  const onGoBack = () => {
+    navigate("/wallet", { replace: true });
   };
 
   useEffect(() => {
@@ -31,6 +39,14 @@ const UserLayout = () => {
   }, []);
 
   useEffect(() => {
+    if (location.pathname.startsWith("/digital-twin-detail")) {
+      setIsShowBackButton(true);
+    } else {
+      setIsShowBackButton(false);
+    }
+  }, [location]);
+
+  useEffect(() => {
     clearTimeout(renewTokenTimeout);
     setRenewTokenTimeout(
       setTimeout(() => {
@@ -40,8 +56,6 @@ const UserLayout = () => {
   }, [accessToken]);
 
   const renewToken = () => {
-    console.log("renewToken");
-
     APIService.renewAccessToken((success: any, json: any) => {
       if (success && json.access_token) {
         dispatch(
@@ -61,9 +75,21 @@ const UserLayout = () => {
   const Header = () => {
     return (
       <div className="flex items-center justify-center">
-        <div className=" flex flex-row w-full max-w-4xl self-center justify-end bg-black h-16">
+        <div className=" flex flex-row w-full max-w-4xl self-center justify-between bg-black h-16 items-center">
+          <IconButton
+            className="ml-2"
+            disabled={!isShowBackButton}
+            onClick={onGoBack}
+          >
+            <HiChevronLeft
+              className={`${
+                isShowBackButton ? "text-white" : "text-black"
+              } font-bold text-4xl`}
+            />
+          </IconButton>
+          <img src="/logo.png" className=" w-14 h-14" alt="logo_header" />
           <IconButton className="mr-2" onClick={onLogout}>
-            <MdLogout className=" text-white font-bold text-4xl" />
+            <MdLogout className="text-white font-bold text-4xl" />
           </IconButton>
         </div>
       </div>
